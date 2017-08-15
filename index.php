8827442fac801b0e1ca53a42ac743852dc3713ef
Unknown <aasish.boy@gmail.com>
2017-08-15 16:03:58 +0545

    new index files
    add

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Our Local Search Engine </title>
+    <link rel="icon" type="img/png" href="image/top.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1.0"> </head>
+
+<body>
+    <nav>
+        <a href="#"> <i class="fa fa-sign-in fa-1x" aria-hidden="true"></i> Login</a>
+        <a href="#"> <i class="fa fa-user-plus" aria-hidden="true"></i> Join_Us</a>
+        <a href="#"> <i class="fa fa-phone" aria-hidden="true"></i> 9815359690</a>
+    </nav>
+    <div id="container">
+        <form action="">
+            <center><img src="img/logo.png" class="img-responsive" alt="search"> </center>
+            <input type="text" class="form-control" name="search" style="width:40%">
+            <input type="text" class="form-control" name="search" style="width:40%">
+            <input type="button" value="search" name="Search" class="btn"> </form>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+<link rel="stylesheet" type="text/css" href="css/own_style.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+</html>
