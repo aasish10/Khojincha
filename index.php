2ffd8b4cc4edae7cc77582a16b2b3061f20e33cc
Unknown <aasish.boy@gmail.com>
2017-08-17 14:46:09 +0545

    Auto addstash before merge of "master" and "origin/master"

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<html>
+	<head>
+		<title>Page Title</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="initial-scale=1.0">
+	</head>
+
+	<body>
+
+	</body>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</html>
