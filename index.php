de869b37e6fdbf61311bc22df6abef27919bba4a
Unknown <aasish.boy@gmail.com>
2017-08-20 07:24:27 +0545

    modify

--- index.php
+++ index.php
@@ -1,30 +1,58 @@
-<!doctype html>
 
 <html>
-	<head>
-		<title>Page Title</title>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="initial-scale=1.0">
-	</head>
+<head>
+	<title>How to create transparent background color navbar bootstrap by seegatesite</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/styles_navbar.css" rel="stylesheet">
+	<link href="css/styles2.css" rel="stylesheet">
+	<link href="font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"> <img alt="Brand" src="image/logo3.png"></a>
+			</div>
 
-	<body>
-	<header>
-	    h
-	</header>
-	
-
-
-	
-	
-	
-<footer>
-    f
-</footer>
-	</body>
-	<script src="js/npm.js">
-    
-    </script>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li><a href="#">IRON MAN</a></li>
+					<li><a href="#">CAPTAIN AMERICA</a></li>
+					<li><a href="#">THOR</a></li>
+					<li><a href="#">HULK</a></li>
+					<li><a href="#">BLACK WIDOW</a></li>
+					<li><a href="#">HAWKEYE</a></li>
+					<li><a href="#">FURY</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">GAME<span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Another action</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Separated link</a></li>
+							<li class="divider"></li>
+							<li><a href="#">One more separated link</a></li>
+						</ul>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Login or Sign up</a></li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+<center><h2 style="color:white">TO DOWNLOAD OR LEARNING HOW TO CREATE THIS BOOTSTRAP THEME PLEASE VISIT : <a href="http://seegatesite.com/easy-way-create-transparent-background-color-navbar-bootstrap-3/" >seegatesite.com</a></h2></center>
+	<script src="js/jquery-1.11.2.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
 </html>
